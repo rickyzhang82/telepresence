62182432f8800ecef8ade9833b1a8e5fe3507656
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-11-14 18:08:00 -0500

    Allow telepresence to run root daemon inside of a docker container
    Signed-off-by: billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>

--- pkg/client/cli/connect/connector.go
+++ pkg/client/cli/connect/connector.go
@@ -333,7 +333,7 @@ func newUserDaemon(ctx context.Context, conn *grpc.ClientConn, daemonID *daemon.
 
 func EnsureUserDaemon(ctx context.Context, required bool) (rc context.Context, err error) {
 	defer func() {
-		if err == nil && required && !daemon.GetUserClient(rc).Containerized() {
+		if err == nil && required &&  !(os.Getuid() == 0 && daemon.GetUserClient(rc).Containerized()) {
 			// The RootDaemon must be started if the UserDaemon was started
 			err = ensureRootDaemonRunning(ctx)
 		}
