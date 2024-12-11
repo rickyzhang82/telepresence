18375bc9ab1137ecb89bfe36577c753e013834a9
Thomas Hallgren <thomas@tada.se>
2024-12-11 18:06:32 +0100

    Fix lint glitch.
    Signed-off-by: Thomas Hallgren <thomas@tada.se>

--- pkg/proc/docker_other.go
+++ pkg/proc/docker_other.go
@@ -8,9 +8,10 @@ import "context"
 func RunningInContainer() bool {
 	return false
 }
+
 func SetRunningInContainer(_ bool) {
 }
 
-func AppendOSSpecificContainerOpts(ctx context.Context, opts []string) ([]string, error) {
+func AppendOSSpecificContainerOpts(_ context.Context, opts []string) ([]string, error) {
 	return opts, nil
 }
